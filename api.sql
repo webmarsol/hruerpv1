f128a2fee147e2ac2a68aa0cfa6c735ab6bb6f68
webmarsol <webmarsol@gmail.com>
2024-12-18 16:24:50 +0530

    Add files via upload

---
+++ api.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 18, 2024 at 10:03 AM
+-- Server version: 5.7.44-log
+-- PHP Version: 8.1.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `trueelem_hworld26n`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oc_api`
+--
+
+CREATE TABLE `oc_api` (
+  `api_id` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `key` text NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `date_added` datetime NOT NULL,
+  `date_modified` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oc_api_session`
+--
+
+CREATE TABLE `oc_api_session` (
+  `api_session_id` int(11) NOT NULL,
+  `api_id` int(11) NOT NULL,
+  `token` varchar(32) NOT NULL,
+  `session_id` varchar(32) NOT NULL,
+  `session_name` varchar(32) NOT NULL,
+  `ip` varchar(40) NOT NULL,
+  `date_added` datetime NOT NULL,
+  `date_modified` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `oc_api`
+--
+ALTER TABLE `oc_api`
+  ADD PRIMARY KEY (`api_id`);
+
+--
+-- Indexes for table `oc_api_session`
+--
+ALTER TABLE `oc_api_session`
+  ADD PRIMARY KEY (`api_session_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `oc_api`
+--
+ALTER TABLE `oc_api`
+  MODIFY `api_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `oc_api_session`
+--
+ALTER TABLE `oc_api_session`
+  MODIFY `api_session_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
